6569c7d619136352b74dbc5c5eda3c296758e0c9
Lukasz <lmienicki@gmail.com>
2019-02-21 17:21:16 +0100

    zad. 1-7

--- src/main/java/com/javagda23/zad2SuperHeroN/HeroStatistics.java
+++ src/main/java/com/javagda23/zad2SuperHeroN/HeroStatistics.java
@@ -3,6 +3,7 @@ package com.javagda23.zad2SuperHeroN;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+
 @Data
 @AllArgsConstructor
 public class HeroStatistics {
@@ -10,27 +11,15 @@ public class HeroStatistics {
     private int attack;
     private int defence;
 
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public int getDefence() {
-        return defence;
-    }
-
-    public void increaseHealth(int amount){
+    public void increaseHealth(int amount) {
         this.health += amount;
     }
 
-    public void increaseAttack(int amount){
+    public void increaseAttack(int amount) {
         this.attack += amount;
     }
 
-    public void increaseDefence(int amount){
+    public void increaseDefence(int amount) {
         this.defence += amount;
     }
 
