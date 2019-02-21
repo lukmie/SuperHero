6f244910c294bbbec8dd1317ccf1f55b0e9eec1a
Lukasz <lmienicki@gmail.com>
2019-02-21 22:17:10 +0100

    zad. 12

--- src/main/java/com/javagda23/zad2SuperHeroN/Main.java
+++ src/main/java/com/javagda23/zad2SuperHeroN/Main.java
@@ -9,9 +9,23 @@ public class Main {
 //        System.out.println(villain1.getHeroStatistics());
 //        System.out.println(superHero1.getHeroStatistics());
 
-        Villain v2 = new Villain("a", new HeroStatistics(10,10,10), TeamType.RED);
+        Villain v2 = new Villain("a", new HeroStatistics(111,10,10), TeamType.RED);
+        Villain v3 = new Villain("b", new HeroStatistics(20,10,10), TeamType.RED);
+        Villain v4 = new Villain("c", new HeroStatistics(311,20,110), TeamType.RED);
+        Villain v5 = new Villain("e", new HeroStatistics(10,20,110), TeamType.RED);
+        Villain v6 = new Villain("f", new HeroStatistics(11,20,110), TeamType.RED);
         Team t1 = new Team(TeamType.RED);
-
+        System.out.println(v2.getPower());
+        System.out.println(v3.getPower());
+        System.out.println(v4.getPower());
+        System.out.println(v5.getPower());
+        System.out.println(v6.getPower());
         System.out.println(t1.addHeroToTeam(v2));
+        System.out.println(t1.addHeroToTeam(v3));
+        System.out.println(t1.addHeroToTeam(v4));
+        System.out.println(t1.addHeroToTeam(v5));
+        System.out.println(t1.addHeroToTeam(v6));
+
+        System.out.println(t1.getTeamLeader());
     }
 }
--- src/main/java/com/javagda23/zad2SuperHeroN/Team.java
+++ src/main/java/com/javagda23/zad2SuperHeroN/Team.java
@@ -3,12 +3,15 @@ package com.javagda23.zad2SuperHeroN;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Data
 public class Team {
     private TeamType type;
     private List<AbstractHero> heroes;
+    private AbstractHero teamLeader;
 
     public Team(TeamType type) {
         this.type = type;
@@ -21,5 +24,21 @@ public class Team {
             return true;
         } else
             return false;
+    }
+
+    public AbstractHero getTeamLeader() {
+        Collections.sort(heroes, new Comparator<AbstractHero>() {
+            @Override
+            public int compare(AbstractHero o1, AbstractHero o2) {
+                if (o1.getPower() > o2.getPower()) {
+                    return -1;
+                } else if (o1.getPower() < o2.getPower()) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+
+        return heroes.get(0);
     }
 }
