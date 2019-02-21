7755717915fac6f214a5d575110d8b01e5c38664
Lukasz <lmienicki@gmail.com>
2019-02-21 19:59:49 +0100

    zad. 9

--- src/main/java/com/javagda23/zad2SuperHeroN/HeroCreator.java
+++ src/main/java/com/javagda23/zad2SuperHeroN/HeroCreator.java
@@ -1,12 +1,72 @@
 package com.javagda23.zad2SuperHeroN;
 
+import java.io.*;
+
 public abstract class HeroCreator {
-    public static AbstractHero createSuperHero(String name, HeroStatistics heroStatistics, TeamType team){
+    public static AbstractHero createSuperHero(){
         return new SuperHero("Jack", new HeroStatistics(60,70,65), TeamType.RED);
     }
 
-    public static AbstractHero createVillain(String name, HeroStatistics heroStatistics, TeamType team){
+    public static AbstractHero createVillain(){
         return new Villain("John", new HeroStatistics(10,10,10), TeamType.BLUE);
+    }
+
+    public static AbstractHero createSuperHeroWithDefaultStats(){
+
+        int health = 0;
+        int attack = 0;
+        int defence = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("applicationProperties.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if(line.equals("SuperHero")){
+                    line = bufferedReader.readLine();
+                    health = Integer.parseInt(line.split("=")[1]);
+
+                    line = bufferedReader.readLine();
+                    attack = Integer.parseInt(line.split("=")[1]);
+
+                    line = bufferedReader.readLine();
+                    defence = Integer.parseInt(line.split("=")[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not exist.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new SuperHero("Jerry", new HeroStatistics(health, attack, defence), TeamType.GREEN);
+    }
+
+    public static AbstractHero createVillainWithDefaultStats(){
+
+
+        int health = 0;
+        int attack = 0;
+        int defence = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("applicationProperties.txt"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if(line.equals("Villain")){
+                    line = bufferedReader.readLine();
+                    health = Integer.parseInt(line.split("=")[1]);
+
+                    line = bufferedReader.readLine();
+                    attack = Integer.parseInt(line.split("=")[1]);
+
+                    line = bufferedReader.readLine();
+                    defence = Integer.parseInt(line.split("=")[1]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not exist.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Villain("Tom", new HeroStatistics(health, attack, defence), TeamType.BLUE);
+
     }
 
 }
--- src/main/java/com/javagda23/zad2SuperHeroN/Main.java
+++ src/main/java/com/javagda23/zad2SuperHeroN/Main.java
@@ -2,6 +2,11 @@ package com.javagda23.zad2SuperHeroN;
 
 public class Main {
     public static void main(String[] args) {
+        AbstractHero villain1 = HeroCreator.createVillainWithDefaultStats();
+//        AbstractHero villain1 = HeroCreator.createVillain();
+        AbstractHero superHero1 = HeroCreator.createSuperHeroWithDefaultStats();
 
+        System.out.println(villain1.getHeroStatistics());
+        System.out.println(superHero1.getHeroStatistics());
     }
 }
